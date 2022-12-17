a79313131c19ffffbe7ce7e40a2274407f3bf8b1
Kamil Koenig <qenick@qenick.home>
2022-12-17 03:50:52 +0100

    test after deploy

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 function Home() {
   return (
     <div className="main-container">
+      Test po deployu
       <h1>Flashcards app demo</h1>
       <h2>This app supports spaced repetition scheme.</h2>
       <p>In demo version time is accelerated 1440 times. One minute in app simulates 24 hours.</p>
